637929d3290b85e92bd1c22d0ab277bcd518cc09
korney <madkorney@gmail.com>
2024-12-04 23:06:56 +0300

    feat: implement day 04 solution

--- src/day-04/index.ts
+++ src/day-04/index.ts
@@ -1,15 +1,18 @@
 import { getInputDataForDay, getTestADataForDay, getTestBDataForDay } from '../util/index.js';
 
-const DAY_NUMBER = 1;
+const DAY_NUMBER = 4;
 const DAY_NUMBER_FORMATTED = DAY_NUMBER.toString(10).padStart(2, '0');
-const TEST_ANSWER_A = 0;
-const TEST_ANSWER_B = 0;
+const TEST_ANSWER_A = 18;
+const TEST_ANSWER_B = 9;
 
-// ======= Day 01 ======
+// ======= Day 04 ======
 
 const transformInputData = (inputData: string[]) => {
-  //parse input if required
-  return inputData;
+  const data: string[][] = [];
+  inputData.forEach((line) => {
+    data.push(line.split(''));
+  })
+  return data;
 };
 
 const taskA = (inputData: string[], option?: string): number => {
@@ -17,10 +20,49 @@ const taskA = (inputData: string[], option?: string): number => {
   const timer = option ? `TaskA ${option}` : 'TaskA';
   console.time(timer);
 
-  const answer = data.length; // your solution here
+  const sizeX = data[0].length - 1;
+  const sizeY = data.length - 1;
+
+  const getXmasQty = (x: number, y: number): number => {
+    if (data[y][x] !== 'X') return 0;
+
+    const XMAS = ['X', 'M', 'A', 'S'];
+    let xmasQty = 0;
+
+    const hasXmas = (x: number, dx: number, y: number,  dy: number) => {
+      if (dx === 0 && dy === 0 ||
+          x + dx * 3 < 0 ||
+          y + dy * 3 < 0 ||
+          x + dx * 3 > sizeX ||
+          y + dy * 3 > sizeY
+         ) return false;
+
+      let itsXmas = true;
+      for (let k = 0; k < 4; k++) {
+        if (itsXmas && data[y + dy * k][x + dx * k] !== XMAS[k]) itsXmas = false;
+      }
+
+      return itsXmas;
+    }
+
+    for (let dy = -1; dy <= 1; dy++) {
+      for (let dx = -1; dx <= 1; dx++) {
+        if (hasXmas(x, dx, y,  dy)) xmasQty += 1;
+      }
+    }
+
+    return xmasQty;
+  };
+
+  let totalXmas = 0;
+  for (let j = 0; j <= sizeY; j++) {
+    for (let i = 0; i <= sizeX; i++) {
+      totalXmas += getXmasQty(i , j);
+    }
+  }
 
   console.timeEnd(timer);
-  return answer;
+  return totalXmas;
 };
 
 const taskB = (inputData: string[], option?: string): number => {
@@ -28,10 +70,31 @@ const taskB = (inputData: string[], option?: string): number => {
   const timer = option ? `TaskB ${option}` : 'TaskB';
   console.time(timer);
 
-  const answer = data.length; // your solution here
+  const sizeX = data[0].length - 1;
+  const sizeY = data.length - 1;
+
+  const isX_Mas = (x: number, y: number) => {
+    if (data[y][x] !== 'A') return false;
+
+    if (x === 0 || y === 0 || x === sizeX || y === sizeY) {
+      return false;
+    }
+
+    const diag1 = data[y -1][x - 1] + data[y][x] + data[y + 1][x + 1];
+    const diag2 = data[y -1][x + 1] + data[y][x] + data[y + 1][x - 1];
+
+    return (diag1 === 'MAS' || diag1 === 'SAM') && (diag2 === 'MAS' || diag2 === 'SAM')
+  }
+
+  let totalXmas = 0;
+  for (let j = 0; j <= sizeY; j++) {
+    for (let i = 0; i <= sizeX; i++) {
+      if (isX_Mas(i, j)) totalXmas += 1;
+    }
+  }
 
   console.timeEnd(timer);
-  return answer;
+  return totalXmas;
 };
 
 try {
